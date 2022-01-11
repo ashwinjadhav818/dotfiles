1906f7a4ffb2ff5821efa40f30bbf3369d52e1cd
Ashwin Jadhav <ajcuber818@gmail.com>
2022-01-11 22:40:33 +0530

    Update user_profile.ps1

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -16,3 +16,4 @@ Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
+Set-PsReadLineOption -PredictionViewStyle ListView
