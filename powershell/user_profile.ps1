9728471977179053385895a46d4ad6ec91b49613
Ashwin Jadhav <ajcuber818@gmail.com>
2022-01-29 21:50:02 +0530

    Updated PowerShell user_profile & atomic theme.

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,12 +1,11 @@
 # Oh My Posh Themes
-oh-my-posh --init --shell pwsh --config ~\.config\powershell\Themes\craftzdog.omp.json | Invoke-Expression
+# oh-my-posh --init --shell pwsh --config ~\.config\powershell\Themes\craftzdog.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~\.config\powershell\Themes\atomic.omp.json | Invoke-Expression
 
 # Modules
 Import-Module -Name Terminal-Icons
 
 # Alias
-Set-Alias vi nvim
-Set-Alias vim nvim
 Set-Alias np C:\Windows\notepad.exe
 Set-Alias ll ls
 Set-Alias g git
@@ -17,3 +16,6 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PsReadLineOption -PredictionViewStyle ListView
+
+# FZF
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
