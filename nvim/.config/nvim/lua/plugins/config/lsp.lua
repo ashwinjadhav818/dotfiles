6bae9f44d981ab7e1e71ad8e7680e72c3c3424ec
ashwinjadhav818 <ashwinjadhav818@gmail.com>
2023-06-27 16:39:46 +0530

    Added the neovim setup back (fixing the commit bug)

---
+++ nvim/.config/nvim/lua/plugins/config/lsp.lua
@@ -0,0 +1,164 @@
+local status, nvim_lsp = pcall(require, "lspconfig")
+if (not status) then return end
+
+local protocol = require('vim.lsp.protocol')
+
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local enable_format_on_save = function(_, bufnr)
+   vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
+   vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup_format,
+      buffer = bufnr,
+      callback = function()
+         vim.lsp.buf.format({ bufnr = bufnr })
+      end,
+   })
+end
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+   --Enable completion triggered by <c-x><c-o>
+   --local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+   --buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+   -- Mappings.
+   local opts = { noremap = true, silent = true }
+
+   -- See `:help vim.lsp.*` for documentation on any of the below functions
+   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+   --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+   buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+   --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+   vim.api.nvim_create_autocmd("CursorHold", {
+      buffer = bufnr,
+      callback = function()
+         local opts_ = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = 'rounded',
+            source = 'always',
+            prefix = ' ',
+            scope = 'cursor',
+         }
+         vim.diagnostic.open_float(nil, opts_)
+      end
+   })
+end
+
+protocol.CompletionItemKind = {
+   '', -- Text
+   '', -- Method
+   '', -- Function
+   '', -- Constructor
+   '', -- Field
+   '', -- Variable
+   '', -- Class
+   'ﰮ', -- Interface
+   '', -- Module
+   '', -- Property
+   '', -- Unit
+   '', -- Value
+   '', -- Enum
+   '', -- Keyword
+   '﬌', -- Snippet
+   '', -- Color
+   '', -- File
+   '', -- Reference
+   '', -- Folder
+   '', -- EnumMember
+   '', -- Constant
+   '', -- Struct
+   '', -- Event
+   'ﬦ', -- Operator
+   '', -- TypeParameter
+}
+
+-- Set up completion using nvim_cmp with LSP source
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
+   vim.lsp.protocol.make_client_capabilities()
+)
+
+nvim_lsp.flow.setup {
+   on_attach = on_attach,
+   capabilities = capabilities
+}
+
+nvim_lsp.tsserver.setup {
+   on_attach = on_attach,
+   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+   cmd = { "typescript-language-server", "--stdio" },
+   capabilities = capabilities
+}
+
+nvim_lsp.jdtls.setup {}
+
+nvim_lsp.sourcekit.setup {
+   on_attach = on_attach,
+}
+
+nvim_lsp.sumneko_lua.setup {
+   on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      enable_format_on_save(client, bufnr)
+   end,
+   capabilities = capabilities,
+   settings = {
+      Lua = {
+         diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' },
+         },
+
+         workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false
+         },
+      },
+   },
+}
+
+local tw_highlight = require('tailwind-highlight')
+nvim_lsp.tailwindcss.setup({
+   on_attach = function(client, bufnr)
+      tw_highlight.setup(client, bufnr, {
+         single_column = false,
+         mode = 'background',
+         debounce = 200,
+      })
+   end,
+   capabilities = capabilities
+})
+
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+   vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = true,
+      update_in_insert = false,
+      virtual_text = { spacing = 4, prefix = "●" },
+      severity_sort = true,
+   })
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+-- Diagnostic symbols in the sign column (gutter)
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+   local hl = "DiagnosticSign" .. type
+   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+vim.diagnostic.config({
+   virtual_text = {
+      prefix = '●'
+   },
+   update_in_insert = true,
+   float = {
+      source = "always", -- Or "if_many"
+   },
+})
---
+++ nvim/.config/nvim/lua/plugins/config/treesitter.lua
@@ -0,0 +1,38 @@
+local status, ts = pcall(require, "nvim-treesitter.configs")
+if (not status) then return end
+
+ts.setup {
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "fish",
+    "json",
+    "yaml",
+    "css",
+    "html",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "python",
+    "graphql",
+    "regex",
+    "typescript",
+    "javascript",
+  },
+  --- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  autotag = {
+    enable = true,
+  },
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
