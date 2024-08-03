32cfc2406de54c9cfd4b9dcc6d561421fc3c5804
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2024-08-03 19:17:43 +0530

    feat: removed the clutter from wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config.default_prog = { pwsh_path, "-NoProfileLoadTime",  "-NoLogo" }
 
 -- UI
 config.window_decorations = "RESIZE"
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "tokyonight"
 config.window_background_opacity = 0.5
 config.win32_system_backdrop = "Acrylic"
 config.colors = {
@@ -24,7 +24,7 @@ config.inactive_pane_hsb = { -- Dim inactive panes
 }
 
 -- Font
-config.font = wezterm.font "JetBrainsMono Nerd Font"
+config.font = wezterm.font "IosevkaCode Nerd Font"
 config.font_size = 12
 
 -- Keybindings
@@ -64,7 +64,7 @@ config.keys = {
             },
             action = wezterm.action_callback(function(window, line)
                 if line then
-                    window:active_tab():set_title(line)
+                    window:set_title(line)
                 end
             end)
         }
@@ -114,13 +114,10 @@ wezterm.on('augment-command-palette', function()
     {
       brief = 'Rename tab',
       icon = 'md_rename_box',
-
       action = act.PromptInputLine {
         description = 'Enter new name for tab',
         action = wezterm.action_callback(function(window, line)
-          if line then
-            window:active_tab():set_title(line)
-          end
+          window:set_title(line)
         end),
       },
     },
@@ -128,76 +125,6 @@ wezterm.on('augment-command-palette', function()
 end)
 
 -- Tab bar
--- Retro
-config.use_fancy_tab_bar = false
-config.status_update_interval = 1000
-config.tab_bar_at_bottom = false
-wezterm.on("update-status", function(window, pane)
-    -- Workspace name
-    local stat = window:active_workspace()
-    local stat_color = "#f7768e"
-    -- It's a little silly to have workspace name all the time
-    -- Utilize this to display LDR or current key table name
-    if window:active_key_table() then
-        stat = window:active_key_table()
-        stat_color = "#7dcfff"
-    end
-    if window:leader_is_active() then
-        stat = "LDR"
-        stat_color = "#bb9af7"
-    end
-
-    local basename = function(s)
-        -- Nothing a little regex can't fix
-        return string.gsub(s, "(.*[/\\])(.*)", "%2")
-    end
-
-    -- Current working directory
-    local cwd = pane:get_current_working_dir()
-    if cwd then
-        if type(cwd) == "userdata" then
-            -- Wezterm introduced the URL object in 20240127-113634-bbcac864
-            cwd = basename(cwd.file_path)
-        else
-            -- 20230712-072601-f4abf8fd or earlier version
-            cwd = basename(cwd)
-        end
-    else
-        cwd = ""
-    end
-
-    -- Current command
-    local cmd = pane:get_foreground_process_name()
-    -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
-    cmd = cmd and basename(cmd) or ""
-
-    -- Left status (left of the tab line)
-    window:set_left_status(wezterm.format({
-        { Foreground = { Color = stat_color } },
-        { Text = "  " },
-        { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-        { Text = " |" },
-    }))
-
-    -- Right status
-    window:set_right_status(wezterm.format({
-        -- Wezterm has a built-in nerd fonts
-        -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
-        { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-        { Text = " | " },
-        { Foreground = { Color = "#e0af68" } },
-        { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-        "ResetAttributes",
-        { Text = " | " },
-    }))
-end)
--- Appearance setting for when I need to take pretty screenshots
 config.enable_tab_bar = false
-config.window_padding = {
-  left = '0.5cell',
-  right = '0.5cell',
-  top = '0.5cell',
-  bottom = '0cell',
-}
 
 return config
