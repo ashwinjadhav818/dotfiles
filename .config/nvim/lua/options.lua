088d29ed1a4add2043e74abc209044f05b810784
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2024-08-13 16:26:36 +0530

    fix(neovim): updated telescope to show hidden items

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -43,7 +43,7 @@ end
 
 -- Neovide Config
 vim.g.neovide_window_blurred = true
-vim.g.neovide_transparency = 0.8
+vim.g.neovide_transparency = 0.9
 vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 
