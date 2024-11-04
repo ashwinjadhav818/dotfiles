5427806f93be0dceed3930d519a0d477515a5085
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2024-11-04 16:11:48 +0530

    update(nvim): updated treej keymap

--- .config/nvim/lua/plugins/treej.lua
+++ .config/nvim/lua/plugins/treej.lua
@@ -3,9 +3,7 @@ return {
     "Wansmer/treesj",
     lazy = true,
     cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
-    keys = {
-      { "gJ", "<cmd>TSJToggle<CR>", desc = "Toggle Split/Join" },
-    },
+    keys = { { "<space>j", "<cmd>TSJToggle<CR>", desc = "Toggle Split/Join" } },
     opts = {
       use_default_keymaps = false
     },
