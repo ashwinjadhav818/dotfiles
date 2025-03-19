bc3e6ca85b8f48922ad6936813b4c6e632a3c822
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-03-19 19:47:08 +0530

    update(powershell): optimized the config to reduce load times

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,11 +1,11 @@
 # PowerShell Customizing software
- oh-my-posh --init --shell pwsh --config ~\.config\powershell\Themes\aura.omp.json | Invoke-Expression
-#Invoke-Expression (&starship init powershell)
-#$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+# oh-my-posh --init --shell pwsh --config ~\.config\powershell\Themes\aura.omp.json | Invoke-Expression
+Invoke-Expression (&starship init powershell)
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 # Modules
-Import-Module -Name Terminal-Icons
+# Import-Module -Name Terminal-Icons
 
 # Alias
 Set-Alias np C:\Windows\notepad.exe
@@ -17,14 +17,8 @@ Set-Alias cmake make
 Set-Alias lg lazygit
 
 # PSReadLine
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 # Set-PsReadLineOption -PredictionViewStyle ListView
-
-# FZF
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # Komorebi
 $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Ashwin\.config\komorebi'
