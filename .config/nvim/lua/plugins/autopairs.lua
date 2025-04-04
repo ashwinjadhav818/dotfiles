3e58b5e72123722cd22d8a949e1dc7139c8f7350
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-04-04 17:23:56 +0530

    update(komorebi): added autotag plugin instead of autopairs and added transparency

--- .config/nvim/lua/plugins/autopairs.lua
+++
@@ -1,5 +0,0 @@
-return {
-    {
-        "windwp/nvim-autopairs",
-    },
-}
---
+++ .config/nvim/lua/plugins/autotag.lua
@@ -0,0 +1,8 @@
+return {
+    {
+        "tronikelis/ts-autotag.nvim",
+        opts = {},
+        -- ft = {}, optionally you can load it only in jsx/html
+        event = "VeryLazy",
+    }
+}
