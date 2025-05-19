44d3c05cb051aeb29f1eda4873de3920e57d86c1
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-05-19 13:12:02 +0530

    update(nvim): reorganized keymaps and added some new ones

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,14 +1,23 @@
 local utils = require("utils")
 
+-- Plugins
 utils.nmap_leader("a", "<cmd>Alpha<CR>", "Alpha")
+utils.nmap_leader("L", "<cmd>Lazy<CR>", "Lazy")
+
+-- Save
 utils.nmap_leader("w", "<cmd>w<CR>", "Save")
-utils.nmap_leader("q", "<cmd>q<CR>", "Quit")
+utils.nmap_leader("qqq", "<cmd>qa<CR>", "Quit All")
+
+-- Clear search highlights
 utils.nmap_leader("h", "<cmd>nohlsearch<CR>", "No Highlight")
+
+-- Text manipulation
 utils.nmap_leader("u", "<cmd>undo<CR>", "Undo")
 utils.nmap_leader("r", "<cmd>redo<CR>", "Redo")
-utils.nmap_leader("L", "<cmd>Lazy<CR>", "Lazy")
+
+-- Window Management
 utils.nmap_leader("=", "<cmd>vertical resize +5<CR>", "Resize +5")
 utils.nmap_leader("-", "<cmd>vertical resize -5<CR>", "Resize -5")
-utils.nmap_leader("v", "<C-W>v", "Split Right")
-utils.nmap_leader("V", "<C-W>s", "Split Below")
+utils.nmap_leader("v", "<C-W>v", "Split vertically")
+utils.nmap_leader("h", "<C-W>s", "Split horizontally")
 utils.nmap_leader("q", "<cmd>q<CR>", "Close Window")
