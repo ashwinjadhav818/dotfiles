4eed82c82842567ef85edc624329e4b8267f9478
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-05-19 13:11:22 +0530

    update(nvim): add ash colorscheme and removed catppuccin colorscheme

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -8,62 +8,16 @@ return {
                 style = "night",
                 sidebars = { "qf", "help", "NvimTree" },
             })
-            vim.cmd([[colorscheme tokyonight]])
+            -- vim.cmd([[colorscheme tokyonight]])
         end,
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
+  {
+        "drewxs/ash.nvim",
         lazy = false,
         priority = 1000,
-        opts = {
-            flavour = "auto", -- latte, frappe, macchiato, mocha
-            background = {    -- :h background
-                light = "latte",
-                dark = "mocha",
-            },
-            transparent_background = true, -- disables setting the background color.
-            show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
-            term_colors = false,           -- sets terminal colors (e.g. `g:terminal_color_0`)
-            dim_inactive = {
-                enabled = false,           -- dims the background color of inactive window
-                shade = "dark",
-                percentage = 0.15,         -- percentage of the shade to apply to the inactive window
-            },
-            no_italic = false,             -- Force no italic
-            no_bold = false,               -- Force no bold
-            no_underline = false,          -- Force no underline
-            styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
-                comments = { "italic" },   -- Change the style of comments
-                conditionals = { "italic" },
-                loops = {},
-                functions = { "italic" },
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-                operators = {},
-                -- miscs = {}, -- Uncomment to turn off hard-coded styles
-            },
-            color_overrides = {},
-            custom_highlights = {},
-            default_integrations = true,
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                treesitter = true,
-                notify = true,
-                mini = {
-                    enabled = true,
-                    indentscope_color = "",
-                },
-                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-            },
-        },
+        config = function()
+            vim.cmd([[colorscheme ash]])
+        end,
     },
     {
         "craftzdog/solarized-osaka.nvim",
@@ -76,5 +30,5 @@ return {
                 floats = "transparent",
             },
         },
-    },
+    }
 }
