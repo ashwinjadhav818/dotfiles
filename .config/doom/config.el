bb55ebf29f76488d24f3decfa4678fb34e76cd0b
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-08-17 09:42:17 +0530

    refactor(doom): major improvements
    * config.el
    ** add divider comments
    ** add org-modern
    ** add collapse keybinds
    
    * init.el
    ** removed useless plugins

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,23 +1,52 @@
 ;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+;; ---- UI ----
 ;; Theme
 (setq doom-theme 'doom-tokyo-night)
-
 ;; Editor
 (setq display-line-numbers-type 'relative)
 
 ;; Font
-(setq doom-font (font-spec :family "Maple Mono NF" :size 14)
-      doom-variable-pitch-font (font-spec :family "Maple Mono NF" :size 13)
-      doom-big-font (font-spec :family "Maple Mono NF" :size 20))
+(setq doom-font (font-spec :family "MonoLisa" :size 16)
+      doom-variable-pitch-font (font-spec :family "MonoLisa" :size 16)
+      doom-big-font (font-spec :family "MonoLisa" :size 22))
 
+(setq
+ ;; Edit settings
+ org-auto-align-tags nil
+ org-tags-column 0
+ org-fold-catch-invisible-edits 'show-and-error
+ org-special-ctrl-a/e t
+ org-insert-heading-respect-content t
+
+ ;; Org styling, hide markup etc.
+ org-hide-emphasis-markers t
+ org-pretty-entities t
+ org-agenda-tags-column 0
+ org-ellipsis "…"
+ )
+
+;; Org Modern
+(after! org-modern
+  (setq org-modern-star "◉"
+        org-modern-stars '("◉" "○" "✸" "✿" "✜" "◆" "▶"))
+  (global-org-modern-mode))
+
+;; You can also place the global mode inside the org module hook
+;; to be even more specific.
+(add-hook 'org-mode-hook #'org-modern-mode)
+;; ---- Editor ---
 ;; Org Mode Configuration
 (setq org-directory "D:/Areas/org/")
+(after! org
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "WORKING(w!)" "WAIT(W@)" "|" "DONE(d!)" "CANC(c@)")))
+  )
 
 ;; Capture templates
 (setq org-capture-templates
       '(("t" "Todo" entry
-         (file+headline "D:/Areas/org/todo.org" "Inbox")
+         (file+headline "D:/Areas/org/inbox.org" "Inbox")
          "* TODO %^{Task}\n:PROPERTIES:\n:CREATED: %U\n:CAPTURED: %a\n:END:\n%?")
         ("e" "Event" entry
          (file+headline "D:/Areas/org/calendar.org" "Events")
@@ -41,16 +70,24 @@
   :ensure t
   :custom
   (org-roam-directory (file-truename "D:/Areas/org/notes/"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
   :config
+  ;; Set up the key bindings
+  (map! :leader
+        (:prefix ("r" . "roam")
+         :desc "Toggle buffer"    "l" #'org-roam-buffer-toggle
+         :desc "Find node"        "f" #'org-roam-node-find
+         :desc "Show graph"       "g" #'org-roam-graph
+         :desc "Insert node"      "i" #'org-roam-node-insert
+         :desc "Capture"          "c" #'org-roam-capture
+         :desc "Dailies today"    "j" #'org-roam-dailies-capture-today))
+
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
   (require 'org-roam-protocol))
+
+;; Collapes Headings
+(map! :leader
+      :desc "Collapse all headings" "o c" #'outline-hide-sublevels
+      :desc "Expand all headings" "o e" #'outline-show-all)
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -15,21 +15,21 @@
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       indent-guides     ; highlighted indent columns
+       ;;indent-guides     ; highlighted indent columns
        ligatures         ; ligatures and symbols to make your code pretty again
-       minimap           ; show a map of the code on the side
+       ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       nav-flash         ; blink cursor line after big motions
-       ;;neotree           ; a project drawer, like NERDTree for vim
+       ;;nav-flash         ; blink cursor line after big motions
+       neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
-       (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       ;;(popup +defaults)   ; tame sudden yet inevitable temporary windows
        smooth-scroll     ; So smooth you won't believe it's not butter
        tabs              ; a tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
+       ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
+       ;;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
 
@@ -132,7 +132,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +roam2 +pretty); organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
@@ -162,7 +162,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       ;;calendar
+       calendar
        ;;emms
        ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
