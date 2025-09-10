d594f3314dffbaabe1ed164d5449877f0849e67e
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-09-10 20:20:17 +0530

    feat(emacs): add org-bullets to improve appearance

--- emacs/.emacs.d/lisp/packages/org-appearance.el
+++ emacs/.emacs.d/lisp/packages/org-appearance.el
@@ -9,16 +9,32 @@
   :ensure t
   :hook (org-mode . org-modern-mode)
   :custom
-  (org-modern-star '("◉" "○" "✸" "✿"))
+  (org-modern-star nil)
+  (org-modern-hide-stars nil)
   (org-modern-block-name '("src" "quote"))
   (org-modern-table nil))
 
-;; Indentation and bullets
-(use-package org-indent
-  :hook (org-mode . org-indent-mode))
+;; Pretty bullets
+(use-package org-bullets-mode
+  :ensure org-bullets
+  :config
+  :hook org-mode)
+
+;; Indentation and bullets (pretty indentation lines)
+(use-package org-modern-indent
+  :straight (org-modern-indent :type git :host github :repo "jdtsmith/org-modern-indent")
+  :after org-modern
+  :hook (org-mode . org-modern-indent-mode))
 
 ;; Nice ellipsis instead of "..."
-(setq org-ellipsis " ▼ ")
+(setq org-ellipsis "  ")
+
+;; Enable number line even with org-indent
+(add-hook 'org-mode-hook
+          (lambda ()
+            (org-indent-mode 1)
+            (display-line-numbers-mode 1)))
 
 (provide 'org-appearance)
 ;;; org-appearance.el ends here
+
