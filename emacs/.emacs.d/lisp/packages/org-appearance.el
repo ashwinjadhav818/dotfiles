7777cc2b270e33d904f77aa92df8df2545d31642
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-09-10 21:00:49 +0530

    refactor(emacs): org-appearance

--- emacs/.emacs.d/lisp/packages/org-appearance.el
+++ emacs/.emacs.d/lisp/packages/org-appearance.el
@@ -1,4 +1,5 @@
 ;;; org-appearance.el --- Doom-like Org appearance -*- lexical-binding: t; -*-
+
 ;;; Commentary:
 ;; Pretty Org-mode visuals, inspired by Doom Emacs.
 
@@ -10,24 +11,21 @@
   :hook (org-mode . org-modern-mode)
   :custom
   (org-modern-star nil)
-  (org-modern-hide-stars nil)
+  (org-modern-hide-stars t)
   (org-modern-block-name '("src" "quote"))
-  (org-modern-table nil))
+  (org-modern-table nil)
+  (org-modern-ellipsis " ? "))
 
 ;; Pretty bullets
-(use-package org-bullets-mode
-  :ensure org-bullets
-  :config
-  :hook org-mode)
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . org-bullets-mode))
 
 ;; Indentation and bullets (pretty indentation lines)
 (use-package org-modern-indent
   :straight (org-modern-indent :type git :host github :repo "jdtsmith/org-modern-indent")
   :after org-modern
   :hook (org-mode . org-modern-indent-mode))
-
-;; Nice ellipsis instead of "..."
-(setq org-ellipsis "  ")
 
 ;; Enable number line even with org-indent
 (add-hook 'org-mode-hook
