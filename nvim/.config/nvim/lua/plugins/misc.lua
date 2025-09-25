4a4e9dfde4d65c4a284466f55a549498de881381
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-09-25 20:17:16 +0530

    refactor(neovim): add autotag

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,14 +1,26 @@
 return {
-    { "xiyaowong/transparent.nvim" },
-    { "sitiom/nvim-numbertoggle" },
-    {
-        "luckasRanarison/tailwind-tools.nvim",
-        name = "tailwind-tools",
-        build = ":UpdateRemotePlugins",
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "neovim/nvim-lspconfig", -- optional
-        },
-        opts = {}                    -- your configuration
-    }
+	{ "xiyaowong/transparent.nvim" },
+	{ "sitiom/nvim-numbertoggle" },
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		name = "tailwind-tools",
+		build = ":UpdateRemotePlugins",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"neovim/nvim-lspconfig", -- optional
+		},
+		opts = {}, -- your configuration
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+			})
+		end,
+	},
 }
