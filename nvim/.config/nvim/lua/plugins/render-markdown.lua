fb453092c87ee75e9497e2ac2859c300c1e4f2f5
A S H⚡ <81229115+ashwinjadhav818@users.noreply.github.com>
2025-10-23 21:30:59 +0530

    refactor(nvim): add preview-markdown plugin

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,15 +1,26 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
-	opts = {
-		heading = {
-			icons = {},
-		},
-		html = {
-			comment = {
-				conceal = false,
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+		opts = {
+			heading = {
+				icons = {},
 			},
+			html = {
+				comment = {
+					conceal = false,
+				},
+			},
+			render_modes = true,
 		},
-		render_modes = true,
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
 	},
 }
