4bb4204d505365e5748798f7f5f6b5cf4a6d0420
A S H <81229115+ashwinjadhav818@users.noreply.github.com>
2025-12-13 23:17:23 +0530

    fix clipboard in nvim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,44 +1,44 @@
 local options = {
-	backup = false, -- creates a backup file
-	clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-	cmdheight = 0, -- more space in the neovim command line for displaying messages
-	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-	conceallevel = 0, -- so that `` is visible in markdown files
-	fileencoding = "utf-8", -- the encoding written to a file
-	hlsearch = true, -- highlight all matches on previous search pattern
-	ignorecase = true, -- ignore case in search patterns
-	mouse = "a", -- allow the mouse to be used in neovim
-	pumheight = 10, -- pop up menu height
-	showmode = false, -- we don't need to see things like -- INSERT -- anymore
-	showtabline = 4, -- always show tabs
-	smartcase = true, -- smart case
-	smartindent = true, -- make indenting smarter again
-	splitbelow = true, -- force all horizontal splits to go below current window
-	splitright = true, -- force all vertical splits to go to the right of current window
-	swapfile = false, -- creates a swapfile
-	termguicolors = true, -- set term gui colors (most terminals support this)
-	timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
-	undofile = true, -- enable persistent undo
-	updatetime = 300, -- faster completion (4000ms default)
-	writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-	expandtab = true, -- convert tabs to spaces
-	shiftwidth = 4, -- the number of spaces inserted for each indentation
-	tabstop = 4, -- insert 4 spaces for a tab
-	cursorline = true, -- highlight the current line
-	number = true, -- set numbered lines
-	relativenumber = true, -- set relative numbered lines
-	numberwidth = 4, -- set number column width to 2 {default 4}
-	guifont = "Maple Mono NF:h12", -- the font used in graphical neovim applications
-	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-	wrap = true, -- display lines as one long line
-	linebreak = true, -- companion to wrap, don't split words
-	scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor
-	sidescrolloff = 8, -- minimal number of screen columns either side of cursor if wrap is `false`
-	whichwrap = "s<>[]hl", -- which "horizontal" keys are allowed to travel to prev/next line
+    backup = false,                       -- creates a backup file
+    clipboard = "unnamedplus",            -- allows neovim to access the system clipboard
+    cmdheight = 0,                        -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0,                     -- so that `` is visible in markdown files
+    fileencoding = "utf-8",               -- the encoding written to a file
+    hlsearch = true,                      -- highlight all matches on previous search pattern
+    ignorecase = true,                    -- ignore case in search patterns
+    mouse = "a",                          -- allow the mouse to be used in neovim
+    pumheight = 10,                       -- pop up menu height
+    showmode = false,                     -- we don't need to see things like -- INSERT -- anymore
+    showtabline = 4,                      -- always show tabs
+    smartcase = true,                     -- smart case
+    smartindent = true,                   -- make indenting smarter again
+    splitbelow = true,                    -- force all horizontal splits to go below current window
+    splitright = true,                    -- force all vertical splits to go to the right of current window
+    swapfile = false,                     -- creates a swapfile
+    termguicolors = true,                 -- set term gui colors (most terminals support this)
+    timeoutlen = 300,                     -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = true,                      -- enable persistent undo
+    updatetime = 300,                     -- faster completion (4000ms default)
+    writebackup = false,                  -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    expandtab = true,                     -- convert tabs to spaces
+    shiftwidth = 4,                       -- the number of spaces inserted for each indentation
+    tabstop = 4,                          -- insert 4 spaces for a tab
+    cursorline = true,                    -- highlight the current line
+    number = true,                        -- set numbered lines
+    relativenumber = true,                -- set relative numbered lines
+    numberwidth = 4,                      -- set number column width to 2 {default 4}
+    guifont = "Maple Mono NF:h12",        -- the font used in graphical neovim applications
+    signcolumn = "yes",                   -- always show the sign column, otherwise it would shift the text each time
+    wrap = true,                          -- display lines as one long line
+    linebreak = true,                     -- companion to wrap, don't split words
+    scrolloff = 8,                        -- minimal number of screen lines to keep above and below the cursor
+    sidescrolloff = 8,                    -- minimal number of screen columns either side of cursor if wrap is `false`
+    whichwrap = "s<>[]hl",                -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 -- Neovide Config
@@ -46,23 +46,8 @@ vim.g.neovide_transparency = 0.4
 -- vim.opt.winblend = 70
 
 -- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
-vim.opt.shortmess:append("c") -- don't give |ins-completion-menu| messages
-vim.opt.iskeyword:append("-") -- hyphenated words recognized by searches
+vim.opt.shortmess:append("c")                   -- don't give |ins-completion-menu| messages
+vim.opt.iskeyword:append("-")                   -- hyphenated words recognized by searches
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
-
--- WSL Clipboard Fix
-vim.g.clipboard = {
-	name = "WslClipboard",
-	copy = {
-		["+"] = "clip.exe",
-		["*"] = "clip.exe",
-	},
-	paste = {
-
-		["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("\r", ""))',
-		["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("\r", ""))',
-	},
-	cache_enabled = 0,
-}
 
 vim.cmd([[let &shellcmdflag = '-s']])
